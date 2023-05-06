56fb3688df8b71b0cf908f8987cd0db3ebdf8f77
Jinxuan <i@cjxol.com>
2023-05-06 16:02:59 +0100

    Update whatever I have

---
+++ disambiguation/src/main.rs
@@ -0,0 +1,26 @@
+trait Pilot {
+    fn fly(&self);
+}
+
+trait Wizard {
+    fn fly(&self);
+}
+
+struct Human;
+
+impl Pilot for Human {
+    fn fly(&self) {
+        println!("This is your captain speaking.");
+    }
+}
+
+impl Wizard for Human {
+    fn fly(&self) {
+        println!("Up!");
+    }
+}
+
+fn main() {
+    let person = Human;
+    person.fly();
+}
